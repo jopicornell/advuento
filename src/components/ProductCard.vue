7fb59beaae32ff293ef58ab34fb6dead87f21ff1
jopicornell <joan.picornell@gmail.com>
2022-12-03 23:07:49 +0100

    Arreglado botón comprar

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -4,7 +4,7 @@
       <img class='object-cover h-48 w-96 rounded-t-lg' src='https://i.dummyjson.com/data/products/5/thumbnail.jpg'
            alt='' />
     </a>
-    <div class='p-5'>
+    <div class='p-5 flex flex-col justify-between'>
       <a href='#'>
         <h5 class='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>Huawei P30</h5>
       </a>
@@ -12,30 +12,32 @@
         Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made
         its way to the UK.
       </p>
-      <a
-        href='#'
-        class='
+    </div>
+    <a
+      href='#'
+      class='
+          w-full
           gap-x-2
           inline-flex
+          justify-center
           items-center
           px-3
           py-2
-          text-sm
+          text-md
           font-medium
           text-center text-white
           bg-blue-700
-          rounded-lg
+          rounded-b-lg
           hover:bg-blue-800
           focus:ring-4 focus:outline-none focus:ring-blue-300
           dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800
         '
-      >
-        Buy
-        <svg class='w-6 h-6' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
-          <path stroke-linecap='round' stroke-linejoin='round' stroke-width='2'
-                d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'></path>
-        </svg>
-      </a>
-    </div>
+    >
+      <svg class='w-6 h-6' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+        <path stroke-linecap='round' stroke-linejoin='round' stroke-width='2'
+              d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'></path>
+      </svg>
+      Buy
+    </a>
   </div>
 </template>
